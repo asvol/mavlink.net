d597e7f9374e2a329c564b7b5039742eb87597f3
asvol <248997@gmail.com>
2018-09-29 20:49:29 +0500

    [Vehicle] Add telemetry and SendCommand method

--- src/Asv.Mavlink/Vehicle/IVehicle.cs
+++ src/Asv.Mavlink/Vehicle/IVehicle.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Asv.Mavlink.V2.Common;
 
 namespace Asv.Mavlink
@@ -9,5 +11,21 @@ namespace Asv.Mavlink
         IRxValue<int> PacketRateHz { get; }
         IRxValue<HeartbeatPayload> Heartbeat { get; }
         IRxValue<SysStatusPayload> SysStatus { get; }
+        IRxValue<GpsRawIntPayload> GpsRawInt { get; }
+        IRxValue<HighresImuPayload> HighresImu { get; }
+        IRxValue<ExtendedSysStatePayload> ExtendedSysState { get; }
+        IRxValue<AltitudePayload> Altitude { get; }
+        IRxValue<BatteryStatusPayload> BatteryStatus { get; }
+        IRxValue<AttitudePayload> Attitude { get; }
+        IRxValue<VfrHudPayload> VfrHud { get; }
+        Task<CommandAckPayload> SendCommand(MavCmd command, float param1, float param2, float param3, float param4, float param5, float param6, float param7, int atteptCount, CancellationToken cancel);
+    }
+
+    public static class VehicleHelper
+    {
+        public static async Task TakeOff(this IVehicle src, float minimumPitch, float yawAngle, float latitude, float longitude, float altitude, CancellationToken cancel)
+        {
+            var a = await src.SendCommand(MavCmd.MavCmdNavTakeoff, minimumPitch, float.NaN, float.NaN, yawAngle, latitude,longitude,altitude,1,cancel).ConfigureAwait(false);
+        }
     }
 }
--- src/Asv.Mavlink/Vehicle/Vehicle.cs
+++ src/Asv.Mavlink/Vehicle/Vehicle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Threading;
 using System.Threading.Tasks;
 using Asv.Mavlink.Port;
@@ -12,10 +13,11 @@ namespace Asv.Mavlink
     {
         public string ConnectionString { get; set; } = "0.0.0.0:14560";
         public int HeartbeatTimeoutMs { get; set; } = 2000;
-        public int SystemId { get; set; } = 254;
-        public int ComponentId { get; set; } = 254;
-        public int TargetSystemId { get; } = 0;
-        public int TargetComponenId { get; } = 0;
+        public byte SystemId { get; set; } = 254;
+        public byte ComponentId { get; set; } = 254;
+        public byte TargetSystemId { get; } = 1;
+        public byte TargetComponenId { get; } = 1;
+        public int CommandTimeoutMs { get; set; } = 30000;
     }
 
     public class Vehicle : IVehicle
@@ -29,11 +31,18 @@ namespace Asv.Mavlink
         private readonly RxValue<int> _packetRate = new RxValue<int>();
         private readonly RxValue<HeartbeatPayload> _heartBeat = new RxValue<HeartbeatPayload>();
         private readonly RxValue<SysStatusPayload> _sysStatus = new RxValue<SysStatusPayload>();
+        private readonly RxValue<GpsRawIntPayload> _gpsRawInt = new RxValue<GpsRawIntPayload>();
+        private readonly RxValue<HighresImuPayload> _highresImu = new RxValue<HighresImuPayload>();
 
         private readonly MavlinkV2Connection _mavlinkConnection;
         protected readonly IObservable<IPacketV2<IPayload>> InputPackets;
         protected readonly TaskFactory TaskFactory;
         protected readonly CancellationTokenSource DisposeCancel = new CancellationTokenSource();
+        private readonly RxValue<VfrHudPayload> _vfrHud = new RxValue<VfrHudPayload>();
+        private readonly RxValue<AttitudePayload> _attitude = new RxValue<AttitudePayload>();
+        private readonly RxValue<BatteryStatusPayload> _batteryStatus = new RxValue<BatteryStatusPayload>();
+        private readonly RxValue<AltitudePayload> _altitude = new RxValue<AltitudePayload>();
+        private readonly RxValue<ExtendedSysStatePayload> _extendedSysState = new RxValue<ExtendedSysStatePayload>();
 
         public Vehicle(VehicleConfig config)
         {
@@ -50,6 +59,84 @@ namespace Asv.Mavlink
             HandleStatistic();
             HandleHeartbeat(config);
             HandleSystemStatus();
+            HandleGps();
+            HandleHighresImu();
+            HandleVfrHud();
+            HandleAttitude();
+            HandleBatteryStatus();
+            HandleAltitude();
+            HandleExtendedSysState();
+        }
+
+        private void HandleExtendedSysState()
+        {
+            InputPackets
+                .Where(_ => _.MessageId == ExtendedSysStatePacket.PacketMessageId)
+                .Cast<ExtendedSysStatePacket>()
+                .Select(_ => _.Payload)
+                .Subscribe(_extendedSysState, DisposeCancel.Token);
+            DisposeCancel.Token.Register(() => _extendedSysState.Dispose());
+        }
+
+        private void HandleAltitude()
+        {
+            InputPackets
+                .Where(_ => _.MessageId == AltitudePacket.PacketMessageId)
+                .Cast<AltitudePacket>()
+                .Select(_ => _.Payload)
+                .Subscribe(_altitude, DisposeCancel.Token);
+            DisposeCancel.Token.Register(() => _altitude.Dispose());
+        }
+
+        private void HandleBatteryStatus()
+        {
+            InputPackets
+                .Where(_ => _.MessageId == BatteryStatusPacket.PacketMessageId)
+                .Cast<BatteryStatusPacket>()
+                .Select(_ => _.Payload)
+                .Subscribe(_batteryStatus, DisposeCancel.Token);
+            DisposeCancel.Token.Register(() => _batteryStatus.Dispose());
+        }
+
+        private void HandleAttitude()
+        {
+            InputPackets
+                .Where(_ => _.MessageId == AttitudePacket.PacketMessageId)
+                .Cast<AttitudePacket>()
+                .Select(_ => _.Payload)
+                .Subscribe(_attitude, DisposeCancel.Token);
+            DisposeCancel.Token.Register(() => _attitude.Dispose());
+        }
+
+        private void HandleVfrHud()
+        {
+            InputPackets
+                .Where(_ => _.MessageId == VfrHudPacket.PacketMessageId)
+                .Cast<VfrHudPacket>()
+                .Select(_ => _.Payload)
+                .Subscribe(_vfrHud, DisposeCancel.Token);
+            DisposeCancel.Token.Register(() => _vfrHud.Dispose());
+        }
+
+        private void HandleHighresImu()
+        {
+            InputPackets
+                .Where(_ => _.MessageId == HighresImuPacket.PacketMessageId)
+                .Cast<HighresImuPacket>()
+                .Select(_ => _.Payload)
+                .Subscribe(_highresImu, DisposeCancel.Token);
+            DisposeCancel.Token.Register(() => _highresImu.Dispose());
+        }
+
+        private void HandleGps()
+        {
+            InputPackets
+                .Where(_ => _.MessageId == GpsRawIntPacket.PacketMessageId)
+                .Cast<GpsRawIntPacket>()
+                .Select(_ => _.Payload)
+                .Subscribe(_gpsRawInt, DisposeCancel.Token);
+
+            DisposeCancel.Token.Register(() => _gpsRawInt.Dispose());
         }
 
         private void HandleSystemStatus()
@@ -59,6 +146,7 @@ namespace Asv.Mavlink
                 .Cast<SysStatusPacket>()
                 .Select(_ => _.Payload)
                 .Subscribe(_sysStatus, DisposeCancel.Token);
+            DisposeCancel.Token.Register(() => _sysStatus.Dispose());
         }
 
         private void HandleStatistic()
@@ -95,6 +183,7 @@ namespace Asv.Mavlink
                 _link.Downgrade();
             }
         }
+
         private void OnHeartBeat(HeartbeatPacket heartbeatPacket)
         {
             _ts.VerifyAccess();
@@ -107,10 +196,75 @@ namespace Asv.Mavlink
         public IRxValue<int> PacketRateHz => _packetRate;
         public IRxValue<HeartbeatPayload> Heartbeat => _heartBeat;
         public IRxValue<SysStatusPayload> SysStatus => _sysStatus;
+        public IRxValue<GpsRawIntPayload> GpsRawInt => _gpsRawInt;
+        public IRxValue<HighresImuPayload> HighresImu => _highresImu;
+        public IRxValue<ExtendedSysStatePayload> ExtendedSysState => _extendedSysState;
+        public IRxValue<AltitudePayload> Altitude => _altitude;
+        public IRxValue<BatteryStatusPayload> BatteryStatus => _batteryStatus;
+        public IRxValue<AttitudePayload> Attitude => _attitude;
+        public IRxValue<VfrHudPayload> VfrHud => _vfrHud;
+
+        public async Task<CommandAckPayload> SendCommand(MavCmd command, float param1, float param2, float param3, float param4, float param5, float param6, float param7, int atteptCount, CancellationToken cancel)
+        {
+            var packet = new CommandLongPacket
+            {
+                ComponenId = _config.ComponentId,
+                SystemId = _config.SystemId,
+                Payload =
+                {
+                    Command = command,
+                    TargetComponent = _config.TargetComponenId,
+                    TargetSystem = _config.TargetSystemId,
+                    Confirmation = 0,
+                    Param1 = param1,
+                    Param2 = param2,
+                    Param3 = param3,
+                    Param4 = param4,
+                    Param5 = param5,
+                    Param6 = param6,
+                    Param7 = param7,
+                }
+            };
+            byte currentAttept = 0;
+            CommandAckPacket result = null;
+            while (currentAttept < atteptCount)
+            {
+                packet.Payload.Confirmation = currentAttept;
+                ++currentAttept;
+                
+                var timeoutCancel = new CancellationTokenSource(_config.CommandTimeoutMs);
+                var linkedCancel = CancellationTokenSource.CreateLinkedTokenSource(cancel, timeoutCancel.Token);
+                try
+                {
+                    var resultTask = InputPackets
+                        .Where(_ => _.MessageId == CommandAckPacket.PacketMessageId)
+                        .Where(_ => _config.TargetComponenId == 0 ||
+                                    _config.TargetComponenId == _.ComponenId)
+                        .Where(_=> _config.TargetSystemId == 0 ||
+                                   _config.TargetSystemId == _.SystemId)
+                        .Cast<CommandAckPacket>()
+                        .Where(_ => _.Payload.TargetComponent == _config.ComponentId)
+                        .Where(_ => _.Payload.TargetSystem == _config.SystemId)
+                        .Take(1)
+                        .ToTask(linkedCancel.Token);
+                    await _mavlinkConnection.Send(packet, cancel).ConfigureAwait(false);
+                    result = await resultTask.ConfigureAwait(false);
+                    break;
+                }
+                catch (TaskCanceledException)
+                {
+                    if (!timeoutCancel.IsCancellationRequested)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return result?.Payload;
+        }
 
         public void Dispose()
         {
-            _sysStatus.Dispose();
+            
             _heartBeat.Dispose();
             _packetRate.Dispose();
             _link.Dispose();
